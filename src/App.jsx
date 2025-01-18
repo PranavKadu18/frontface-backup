c40f39e483c28f10ef2cd40a4de586c493b0cdc2
sham <sham@gmail.com>
2025-01-19 01:18:54 +0530

    scroll added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Interface from './components/Interface'
+import LocomotiveScroll from 'locomotive-scroll';
+
 
 const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className='w-full h-screen'>
       <Interface />
